6500f0d274206558feea3e4825b561670053859e
aorazalin <dorazalin.alibek@gmail.com>
2021-07-20 21:45:11 +0800

    Creating breakpoint class

---
+++ src/breakpoint.cpp
@@ -0,0 +1,20 @@
+#include <sys/ptrace.h>
+
+#include "breakpoint.hpp"
+#include "helper.hpp"
+
+void Breakpoint::enable() {
+    auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
+    m_saved_data = static_cast<uint8_t>(data & 0xff); // bottom byte 
+    uint64_t int3 = INTERRUPT_COMMAND;
+    uint64_t data_with_int3 = ((data & ~0xff) | int3); // set bottom byte to 0xcc
+    ptrace(PTRACE_POKEDATA, m_pid, m_addr, data_with_int3);
+    m_enabled = true;
+}
+
+void Breakpoint::disable() {
+    auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
+    auto restored_data = ((data & ~0xff) | m_saved_data);
+    ptrace(PTRACE_POKEDATA, m_pid, m_addr, restored_data);
+    m_enabled = false;
+}
