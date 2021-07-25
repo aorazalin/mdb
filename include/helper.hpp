e32eac425c0d35782de9d5efb9a32b0772904a14
aorazalin <dorazalin.alibek@gmail.com>
2021-07-25 15:35:16 +0800

    Fixing typo in helper.cpp

--- include/helper.hpp
+++ include/helper.hpp
@@ -4,6 +4,6 @@
 
 const uint64_t INTERRUPT_COMMAND = 0xcc;
 
-std::vector<std::string> split(const std::string, char);
+std::vector<std::string> split(const std::string &, char);
 
 bool is_prefix(const std::string &, const std::string &);
